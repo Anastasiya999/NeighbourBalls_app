b1e98c0c35abff6d24a436e6266e73ffda1580ea
Anastasiya999 <anapechko@gmail.com>
2021-02-10 19:53:45 +0100

    deleted folder

--- MandelbrotSet/src/complex/Field.java
+++
@@ -1,9 +0,0 @@
-package complex;
-
-public interface Field<T> {
-    T add(T x);         // dodawanie
-    T sub(T x);          // odejmowanie
-    T mul(T x);          // mnożenie
-    T div(T x) throws DivideByZeroException;          // dzielenie
-    String toString();
-}
--- MandelbrotSet/src/mandelbrot/ComplexDrawable.java
+++
@@ -1,7 +0,0 @@
-package mandelbrot;
-import complex.Complex;
-import javafx.scene.image.PixelWriter;
-
-public interface ComplexDrawable {
-    void draw(PixelWriter pw, Complex a, Complex b, int w, int h);
-}
